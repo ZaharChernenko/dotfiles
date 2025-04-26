b28dc3084f9e09c832cd0d210b73ae481f503d7e
ZaharChernenko <zaharchernenko35@gmail.com>
2025-04-26 14:32:18 +0300

    added: vscode macbook settings and keybindings

--- ycm_extra_conf/completers/__init__.py
+++ ycm_extra_conf/completers/__init__.py
@@ -1,6 +1,3 @@
-import os
-import sys
-
 from completers.ICompleter import ICompleter
 from completers.TCppCompleter import TClangCompleter, TClangdCompleter
 from completers.TPythonCompleter import TPythonCompleter
